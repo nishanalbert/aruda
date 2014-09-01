458a718d851f250bbc8aab4ed892006012e6a242
srinath23 <ussrinath_123@yahoo.com>
2014-09-01 17:33:59 +0530

    Changed structure to include header and templates, contact page

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>Aruda Glass Solutions</title>
+<meta name="keywords"
+	content="Aruda Glass Solutions, Aruda Glass Solutions" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<script src="js/jquery-1.11.1.js"></script>
+<script src="js/init.js"></script>
+<!-- bxSlider Javascript file -->
+<script src="slider/jquery.bxslider.min.js"></script>
+
+<link href="css/style.css" rel="stylesheet" />
+<!-- bxSlider CSS file -->
+<link href="slider/jquery.bxslider.css" rel="stylesheet" />
+
+
+</head>
+<body>
+<div id='main-content'>
+	<!-- Nav -->
+	<?php
+    	include "header.html";
+	?>
+
+	<div id='content'>
+		<div id="slider">
+			<ul class="bxslider">
+				<li><img src="images/aruda1.jpg" /></li>
+				<li><img src="images/aruda2.jpg" /></li>
+				<li><img src="images/aruda3.jpg" /></li>
+				<li><img src="images/aruda4.jpg" /></li>
+				<li><img src="images/aruda2.jpg" /></li>
+				<li><img src="images/aruda3.jpg" /></li>
+				<li><img src="images/aruda4.jpg" /></li>
+			</ul>
+
+		</div>
+		<div>
+
+			<h1 style='text-align:center'>Welcome to Aruda Glass Solutions</h1>
+
+			<p style='text-align:center'>Aruda Glass Solutions is a service portal which offeres a wide
+				array of solutions both to businesses and customers. you have a
+				problem and Aruda Glass Solutions has a solution.</p>
+		</div>
+	</div>
+	<?php include "footer.html"; ?>
+</div>
+</body>
+</html>
