5f87526a5af528c57e9596a4e3403d8ff8207d83
ckr1989 <ckr1989@gmail.com>
2014-09-08 01:11:07 +0530

    Base for Gallery page. Changes still pending.

---
+++ gallery.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Aruda Glass Solutions</title>
+<meta name="keywords"
+	content="Aruda Glass Solutions, Aruda Glass Solutions" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<script src="js/jquery-1.11.1.js"></script>
+<script src="js/init.js"></script>
+<!-- bxSlider Javascript file -->
+<script src="slider/jquery.bxslider.min.js"></script>
+
+<link href="css/style.css" rel="stylesheet" />
+
+</head>
+<body>
+<div id='main-content'>
+	<!-- Nav -->
+	<?php
+    	include "header.html";
+	?>
+
+	<div id='content'>
+		<p id="gallery-header">These are some of our works at various Client sites</p>
+		
+		<div id="gallery-block">
+			<img alt="ITC Gardenia" src="images/gallery1.jpg" />
+			<div>
+				<h1>ITC Gardenia, Bangalore</h1>
+				<p>We specialise in installation of Glasses.
+				   We have pioneered in this field from 12 years.
+				   What an important client he was
+				   Some Random Text for taking up Project with AGS.</p>
+				<p><a href="">read more</a></p>
+			</div>
+		</div>
+		
+		<div id="gallery-block">
+			<img alt="ITC Gardenia" src="images/gallery2.jpg" />
+			<div>
+				<h1>Mr. Ravi Shankar's Home, Chennai</h1>
+				<p>We specialise in installation of Glasses.
+				   We have pioneered in this field from 12 years.
+				   What an important client he was
+				   Some Random Text for taking up Project with AGS.</p>
+				<p><a href="">read more</a></p>
+			</div>
+		</div>
+	</div>
+	<?php include "footer.html"; ?>
+</div>
+</body>
+</html>
